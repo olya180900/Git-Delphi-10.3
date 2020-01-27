43d9c20634dce54540d53dd94e6a3360e9039c32
ahmatnurovaolya@yandex.ru <olya180900>
2020-01-27 13:12:56 +0500

    dfsf

---
+++ HelloWorld/HelloWorldProject.dpr
@@ -0,0 +1,14 @@
+program HelloWorldProject;
+
+uses
+  Vcl.Forms,
+  HelloWorld in 'HelloWorld.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
